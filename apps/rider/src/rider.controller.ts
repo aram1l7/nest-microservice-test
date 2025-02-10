38f64f85f8e17226d03fe9023fc4a7b5a62e162c
HaiderMalik12 <haidermalik504@gmail.com>
2025-02-11 00:22:31 +0500

    implemented tcp based communication

---
+++ apps/rider/src/rider.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { RiderService } from './rider.service';
+import { MessagePattern } from '@nestjs/microservices';
+
+@Controller('rider')
+export class RiderController {
+  constructor(private readonly riderService: RiderService) {}
+
+  // @Get(':id')
+  @MessagePattern({ cmd: 'get-rider' })
+  async getRiderById(
+    // @Param()
+    // params: any
+    data: any
+  ) {
+    console.log(data)
+    return Promise.resolve({ id: data.id, firstName: 'Jane', lastName: 'Doe', email: 'jane@gmail.com' })
+  }
+}
